2822d475341007b9fe9d8f6f56833923a6ff5166
Nandani-koli <70843941+Nandani-koli@users.noreply.github.com>
2023-01-19 19:49:38 +0530

    Update config.js

--- model/config.js
+++ model/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-const db = "mongodb+srv://nandanikoli:4H4g6csLz8uP9iRx@cluster0.5uacygb.mongodb.net/ClassicFashionBlog?retryWrites=true&w=majority";
+const db = "mongodb+srv://nandanikoli:{password}@cluster0.5uacygb.mongodb.net/ClassicFashionBlog?retryWrites=true&w=majority";
 
 mongoose.connect(db).then(() => {
     console.log("Connection successful");
